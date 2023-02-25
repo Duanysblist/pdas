59f4945247b5b9139686f42624eb22e4c43336e7
Daniel Duany <dduany@email.wm.edu>
2023-02-24 21:08:12 -0500

    Prop functionality

---
+++ src/ProfileCard.js
@@ -0,0 +1,16 @@
+function ProfileCard({ title, handle }) {
+  //   const title = props.title;
+  //   const handle = props.handle;
+
+  //   const {title, handle} = props;
+  // Javascript destructuring
+
+  return (
+    <div>
+      <div>Title is {title} </div>
+      <div>Handle is {handle} </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
